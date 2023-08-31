a4c6dbab927b33b34825afea0c7619993ae13f45
n-levy <nirlevy5@gmail.com>
2023-09-01 01:00:56 +0200

    Update test.py

--- scripts/testing/test.py
+++ scripts/testing/test.py
@@ -24,14 +24,81 @@ exec(functions)
 
 """
 Now we train the algorithm.
-This will happen once a day.
+This should happen once a day.
+There are two training functions, one for visitors and one for users.
+The function below is for visitors. It produces a dataframe that is later used by the functions below that recommend items to visitors.
 """
-
 # train the algorithm for providing recommendations to visitors 
 exec(train_visitors)
+print(df_items_with_betas.info())
+
+"""
+If df_items_with_betas was created, then the function was probably successful.
+"""
+
+"""
+The function below produces a dataframe that is later used by the functions below that recommend items to users.
+"""
 
 # train the algorithm for providing recommendations to users 
 exec(train_users)
+print(df_users_items_with_betas.info())
 
+"""
+If df_users_items_with_betas was created, then the function was probably successful.
+"""
 
+"""
+We are now ready to test providing a recommendation to a visitor.
+"""
+# run the script for choosing one item for an online visitor
+exec(choose_item_online_visitor)
 
+# examine the results
+print(df_results)
+
+"""
+The output is supposed to be a dataframe that contains:
+    a. The modelid (The code randomly chooses one model out of two possibilites, the purpose is to A/B test the recommendation model)
+    b. The model name
+    c. The itemid of the recommended item
+If this is indeed the output, then the test is successful.
+"""
+
+"""
+We now test the script that provides a recommendation to an online visitor.
+For the test, we choose a random userid. Note that in production, the userid will be provided by the PWA.
+"""
+# Randomly choose one userid
+df_users_requesting_recommendation = create_users_requesting_recommendation(df_users, 1)
+# make sure that it worked properly
+print(df_users_requesting_recommendation)
+
+# choose an item for this user
+exec(choose_item_online_user)
+print(df_results)
+
+"""
+The output is supposed to be a dataframe that is similar to the one described above. 
+If this is indeed the output, then the test is successful.
+"""
+
+"""
+The final tests is for mutliple users who chose to receive recommendations
+via email or mobile phone. This should run once a day.
+Similar to the test above, we choose the userids randomly but in production they will be provided by the PWA.
+"""
+# Randomly choose multiple userids
+df_users_requesting_recommendation = create_users_requesting_recommendation(df_users, 5)
+# make sure that it worked properly
+print(df_users_requesting_recommendation)
+
+# choose an item for this user
+exec(choose_items_many_offline_users)
+print(df_results)
+
+"""
+The output should be a dataframe that has the same columns as the previous ones,
+but mutliple rows (one for each user).
+If this is indeed the output, then the test is successful.
+"""
